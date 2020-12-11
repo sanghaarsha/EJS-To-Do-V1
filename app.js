ac86827344caf7660e00e3a1d869e3e3e31893ad
sanghaarsha <sanghaarsha@gmail.com>
2020-12-12 00:25:07 +0545

    work list added at /work

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -10,9 +14,11 @@ let port = process.env.PORT;
 if (port == null || port == "") {
     port = 3030;
 }
-
+// Global array for list-items of To-Do
 let todos = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
+// For default List
 app.get("/", (req, res) => {
     let date = new Date();
 
@@ -24,13 +30,30 @@ app.get("/", (req, res) => {
 
     let day = date.toLocaleDateString("en-US", options);
 
-    res.render("list", { day: day, todoArray: todos });
+    res.render("list", {
+        listTitle: day,
+        todoArray: todos,
+    });
+});
+
+// For woork list
+app.get("/work", (req, res) => {
+    res.render("list", {
+        listTitle: "work",
+        todoArray: workItems,
+    });
 });
 
 app.post("/", (req, res) => {
-    newTodo = req.body.newTodo;
-    todos.push(newTodo);
-    res.redirect("/");
+    let newTodo = req.body.newTodo;
+
+    if (req.body.list === "work") {
+        workItems.push(newTodo);
+        res.redirect("/work");
+    } else {
+        todos.push(newTodo);
+        res.redirect("/");
+    }
 });
 
 app.listen(port);
